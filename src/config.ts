3d71161fc52e0e88310ab6fb64babe63fde71b33
Reem Al-Emadi <reem@Reems-MacBook.local>
2018-04-02 04:02:23 +0300

    “comment”

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 //config constants
 export const WORDPRESS_URL = 'http://essence-of-you.000webhostapp.com/';
-export const WORDPRESS_REST_API_URL = WORDPRESS_URL + 'wp-json/wp/v2/pages/9';
+export const WORDPRESS_REST_API_URL = WORDPRESS_URL + 'wp-json/wp/v2/';
